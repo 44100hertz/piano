a4a52a25e245f1dc76915ee3e45950924b685bcf
Sam Pagenkopf <ssaammp@gmail.com>
2017-05-02 10:20:00 -0500

    less silly envelope function (given known data)

--- src/instr.c
+++ src/instr.c
@@ -9,13 +9,13 @@ double instr_env_get(Note* note)
 {
     switch(note->key_state) {
     case KEY_OFF: return 0;
-    case KEY_HELD: return fmin(note->age / 20.0, 1.0);
-    case KEY_RELEASE: return fmax(1 - (note->age / 80.0), 0);
+    case KEY_HELD: return fmax(1 - (note->age / 80.0), 0);
+    case KEY_RELEASE: return fmax(1 - (note->age / 80.0), 0) * 0.5;
     default: fprintf(stderr, "invalid note state: %d\n", note->key_state); return 0;
     }
 }
 
 float instr_get(Note* note, long srate)
 {
-    return wave_sine(note->phase * INT16_MAX / srate / PP) * note->vol;
+    return wave_sine(note->phase * INT16_MAX / srate / PP) * note->rampvol;
 }
